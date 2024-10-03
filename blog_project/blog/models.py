5e4e9fee5f594b4cfe33c949462c732c700273f6
lost-in-noise <nancykikvadze@gmail.com>
2024-10-03 21:00:05 +0400

    Add verbose_name to Post model fields for better clarity in admin panel

--- blog_project/blog/models.py
+++ blog_project/blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)  # Title of the blog post
-    content = models.TextField()  # Content of the blog post
-    created_at = models.DateTimeField(auto_now_add=True)  # Date and time when the post was created
-    category = models.CharField(max_length=100, blank=True)  # Category associated with the post
+    title = models.CharField(max_length=200, verbose_name="Blog Title")  # Title of the blog post
+    content = models.TextField(verbose_name="Post Content")  # Content of the blog post
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created Date")  # Date and time when the post was created
+    category = models.CharField(max_length=100, blank=True, verbose_name="Post Category")  # Category associated with the post
 
     def __str__(self):
         return self.title
